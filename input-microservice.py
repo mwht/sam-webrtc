440ccd01281cc33201f36770b2acff8ffc99d9ec
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-06-13 16:14:40 +0200

    add sending keys to OS

--- input-microservice.py
+++ input-microservice.py
@@ -1,6 +1,32 @@
-import asyncio
-import websockets
-from pynput.keyboard import Key, Controller
+#!/usr/bin/env python
 
-if __name__ == '__main__':
-	pass
+import asyncio
+import json
+from pynput.keyboard import Key, Controller
+import websockets
+
+keyboard = Controller()
+
+async def handle_input(websocket, path):
+	while True:
+		input = await websocket.recv()
+		
+		data = json.loads(input)
+		
+		print(data)
+		
+		try:
+			if data['type'] == "keyboard":
+				key = data['event']['eventData']
+				if data['event']['eventType'] == "down":
+					keyboard.press(key)
+				elif data['event']['eventType'] == "up":
+					keyboard.release(key)
+		except ValueError:
+			pass
+				
+
+start_server = websockets.serve(handle_input, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
